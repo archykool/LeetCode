30c0fc7d1db41056068ad51bfc88c2a0ef122f49
Archy <34702275+archykool@users.noreply.github.com>
2025-01-14 21:33:39 -0500

    LC-squares-of-a-sorted-array [Runtime: 0 ms; Memory: 30.4 MB]

--- two-pointers/squares-of-a-sorted-array.cpp
+++ two-pointers/squares-of-a-sorted-array.cpp
@@ -1,10 +1,20 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        for (int i = 0; i < nums.size(); i++) {
-            nums[i] *= nums[i];
+        vector<int> res(nums.size(), 0);
+        int k = nums.size() - 1;
+        
+        for (int i = 0, j = nums.size() - 1; i <= j;) {
+            if(nums[i] * nums[i] < nums[j] * nums[j]){
+                res[k] = nums[j] * nums[j];
+                j--;
+                k--;
+            } else {
+                res[k] = nums[i] * nums[i];
+                k--;
+                i++;
+            }
         }
-        sort (nums.begin(), nums.end());
-        return nums;
+        return res;
     }
 };
