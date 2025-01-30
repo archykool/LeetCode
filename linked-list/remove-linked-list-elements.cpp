9d487c3867d5d22b1c3c34f3b66172f968e4d440
Archy <34702275+archykool@users.noreply.github.com>
2025-01-30 12:02:20 -0500

    LC-remove-linked-list-elements [Runtime: 6 ms; Memory: 21.1 MB]

--- linked-list/remove-linked-list-elements.cpp
+++ linked-list/remove-linked-list-elements.cpp
@@ -11,20 +11,17 @@
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-        ListNode* dummyHead = new ListNode(0);
-        dummyHead->next = head;
-        ListNode* cur = dummyHead;
-        while (cur->next != NULL){
-            if (cur -> next -> val == val){
-                ListNode* tmp = cur->next;
-                cur->next = cur->next->next;
-                delete tmp;
-            } else {
-                cur = cur->next;
-            }
+        if (head == nullptr) {
+            return nullptr;
         }
-        head = dummyHead->next;
-        delete dummyHead;
-        return head;
+
+        if (head->val == val) {
+            ListNode* newHead = removeElements(head->next, val);
+            delete head;
+            return newHead;
+        } else {
+            head->next = removeElements(head->next, val);
+            return head;
+        }
     }
 };
