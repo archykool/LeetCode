a0da3260b37ac11c63e75fc99ecaede2c93fbf6b
Archy <34702275+archykool@users.noreply.github.com>
2025-03-17 20:17:39 -0400

    LC-palindrome-linked-list [Runtime: 0 ms; Memory: 117.9 MB]

---
+++ two-pointers/palindrome-linked-list.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        ListNode* temp1 = head;
+        ListNode* temp2 = head;
+
+        while(temp1->next && temp1->next->next){
+            temp1 = temp1->next->next;
+            temp2 = temp2->next;
+        }
+        ListNode* curr = temp2->next;
+        ListNode* prev = nullptr;
+
+        while(curr){
+            ListNode* temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        while(prev && head){
+            if(prev->val != head->val) return false;
+            prev = prev->next;
+            head = head->next;
+        }
+        return true;
+    }
+};
