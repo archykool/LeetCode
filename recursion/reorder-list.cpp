933d313dcdd0107f86cf4866353444c207252f78
Archy <34702275+archykool@users.noreply.github.com>
2025-03-17 21:17:28 -0400

    LC-reorder-list [Runtime: 0 ms; Memory: 23.9 MB]

--- recursion/reorder-list.cpp
+++ recursion/reorder-list.cpp
@@ -11,20 +11,22 @@
 class Solution {
 public:
     void reorderList(ListNode* head) {
-        if((!head) || (!head->next->next))  return;
-
+        if ((!head) || (!head->next) || (!head->next->next)) return; // Edge cases
+        
         stack<ListNode*> my_stack;
         ListNode* ptr = head;
         int size = 0;
-        while(ptr != NULL){
+        while (ptr != NULL) // Put all nodes in stack
+        {
             my_stack.push(ptr);
             size++;
             ptr = ptr->next;
         }
-
+        
         ListNode* pptr = head;
-        for(int j = 0; j < size/2; j++){
-            ListNode* element = my_stack.top();
+        for (int j=0; j<size/2; j++) // Between every two nodes insert the one in the top of the stack
+        {
+            ListNode *element = my_stack.top();
             my_stack.pop();
             element->next = pptr->next;
             pptr->next = element;
