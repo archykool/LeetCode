0655c012cbc1add4f55b3029b30eaf6c95d3ad41
Archy (Yaqi) Guo <34702275+archykool@users.noreply.github.com>
2026-03-01 14:22:43 -0500

    LC-game-play-analysis-iv [Runtime: 588 ms; Memory: 0B]

--- database/game-play-analysis-iv.sql
+++ database/game-play-analysis-iv.sql
@@ -1,12 +1,15 @@
 # Write your MySQL query statement below
-SELECT
-    ROUND(
-        COUNT(a.player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2
-        ) AS fraction
-FROM Activity AS a
-JOIN (
-    SELECT player_id, MIN(event_date) AS first_login
+WITH FirstLogin AS(
+    SELECT
+        player_id,
+        MIN(event_date) AS first_date
     FROM Activity
     GROUP BY player_id
-) AS fli ON a.player_id = fli.player_id 
-      AND DATEDIFF(a.event_date, fli.first_login) = 1;
+)
+
+SELECT
+    ROUND(COUNT(f.player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) AS fraction
+FROM FirstLogin AS f
+JOIN Activity AS a
+    ON f.player_id = a.player_id 
+    AND DATEDIFF(a.event_date, f.first_date) = 1;
