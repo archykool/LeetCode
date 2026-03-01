ecfa243523b34ef2cdfb6720315af9258c0c1ce2
Archy (Yaqi) Guo <34702275+archykool@users.noreply.github.com>
2026-02-28 21:54:48 -0500

    LC-project-employees-i [Runtime: 533 ms; Memory: 0B]

---
+++ database/project-employees-i.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT 
+    p.project_id,
+    ROUND((SUM(e.experience_years) / COUNT(DISTINCT(p.employee_id))), 2) AS average_years
+FROM Project AS p
+LEFT JOIN Employee AS e
+ON p.employee_id = e.employee_id
+GROUP BY p.project_id;
