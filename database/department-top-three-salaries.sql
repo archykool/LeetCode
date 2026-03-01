68ae56ef7133b24476ef21324475e55b5ede2f65
Archy (Yaqi) Guo <34702275+archykool@users.noreply.github.com>
2026-03-01 14:11:05 -0500

    LC-department-top-three-salaries [Runtime: 1002 ms; Memory: 0B]

--- database/department-top-three-salaries.sql
+++ database/department-top-three-salaries.sql
@@ -1,17 +1,16 @@
 # Write your MySQL query statement below
-WITH RankSalaryTop3 AS(
+WITH RankSalaries AS(
     SELECT
         d.name AS Department,
         e.name AS Employee,
         e.salary AS Salary,
-        DENSE_RANK() OVER(PARTITION BY d.name ORDER BY e.salary DESC) AS rnk
-    FROM Employee AS e
-    LEFT JOIN Department AS d ON e.departmentId = d.id
+        DENSE_RANK() OVER(PARTITION BY e.departmentId ORDER BY e.salary DESC) AS rnk
+    FROM Employee AS e 
+    JOIN Department AS d ON e.departmentId = d.id
 )
 
 SELECT
     Department,
     Employee,
     Salary
-FROM RankSalaryTop3
-WHERE rnk <= 3;
+FROM RankSalaries WHERE rnk <= 3;
